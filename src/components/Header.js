ef583632efcda36bb143d9f9677a47927e60dc98
kiizerd <ajsewell13@gmail.com>
2022-06-06 00:59:32 -0500

    Tweak header color and spacing.

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,13 +13,15 @@ const MyHeader = () => {
       pb={5}
       sx={{
         display: "flex",
-        backgroundColor: dark ? theme.colors.dark[8] : theme.colors.blue[3],
+        backgroundColor: dark ? theme.colors.dark[8] : theme.colors.blue[2],
         justifyContent: "space-around",
         flexWrap: "wrap",
         borderColor: dark ? theme.colors.dark[3] : theme.colors.dark[8],
       }}
     >
-      <Title order={3}>Electronics and Accessories</Title>
+      <Title order={3} mr={6}>
+        Electronics and Accessories
+      </Title>
       <Navbar />
     </Header>
   );
